4a222057855faea446d10f2cb4e728ad6cab4fe1
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-27 14:52:25 -0400

    added fetching functionality to service worker, now it serves static assets offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,3 +39,37 @@ self.addEventListener("activate", function (evt) {
 
     self.clients.claim();
 });
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    // cache succesfully requests to the API
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                    .then(response => {
+                        // If response is good, clone it and store in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch( err => {
+                        //Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+
+    //if request is not for the API, serve static assets using 'offline-first' approach.
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
+        })
+    );
+});
